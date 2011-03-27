e4b956023c6b474afd9cb73c5bc033cdf1688640
John Barker <jebarker@gmail.com>
2011-03-27 12:36:57 +0800

    Handle new format produced by papers

--- bibliography/fix-papers.rb
+++ bibliography/fix-papers.rb
@@ -9,11 +9,17 @@
 # Bibtex uses months in abbrev {Jan} -> {Dec}
 #
 MONTHS = %w[Jan Feb Mar Apr May June July Aug Sept Oct Nov Dec]
+
+def find_month(text)
+  MONTHS.each_with_index do |month, index|
+    return (index + 1) if month =~ /\A#{month}/i
+  end
+end
+
 File.open(ARGV[0]) do |file|
   file.each do |line|
-    matches = line.match(/month\s*=\s*\{([A-Za-z]+)\}/)
-    if matches #and MONTHS.include?(matches[1])
-      index = MONTHS.index(matches[1]) + 1
+    matches = line.match(/month\s*=\s*\{([A-Za-z]+)\}/i)
+    if matches && (index = find_month(matches[1]))
       puts "#{line[0..(matches.offset(1)[0] - 1)]}#{index}#{line[(matches.offset(1)[1])..-1]}"
     else
       puts line
